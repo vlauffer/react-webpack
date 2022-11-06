db6f8f2cb737ed9c117eab826d086e5fd2942556
Vince Lauffer <vincelauffer@Vinces-MacBook-Pro.local>
2022-11-05 22:20:27 -0400

    first

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    port: 9050,
+    static: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+  })
+]
+    
+    
+};
